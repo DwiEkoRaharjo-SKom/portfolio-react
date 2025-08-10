23bc5697cb46a26503bd4a1ae2293007072291ab
DwiEkoRaharjo-SKom <dwieko.comp.cell@gmail.com>
2025-08-10 07:30:49 +0700

    fix: add basename for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';  // Impor BrowserRouter
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
+root.render(
+  <BrowserRouter basename={import.meta.env.BASE_URL}> {/* Tambahkan basename */}
+    <App />
+  </BrowserRouter>
+);
